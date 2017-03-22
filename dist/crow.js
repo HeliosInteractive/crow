502d13c74d2677359fdfa732f5f18edcdc7bdaf7
Adam McFadden <adam.mcfadden@heliosinteractive.com>
2017-03-22 13:26:27 -0700

    Create methods for corresponding log levels within crow

--- dist/crow.js
+++ dist/crow.js
@@ -25,22 +25,60 @@
     }
 
   })(function() {
-  var request = function(uri, body) {
-    "use strict";
-    console.log("request called");
-    return console.log("got to the end"), function(uri, body) {
-        console.log("in xhr");
+  "use strict";
+
+var request = function(uri, body) {
+    return function(uri, body) {
         var oReq = new XMLHttpRequest();
         oReq.open("POST", uri), oReq.setRequestHeader("Content-Type", "application/json"), 
         oReq.send(JSON.stringify(body));
     }(uri, body);
-}, request, crow = function(application, level, message) {
-    "use strict";
-    console.log(request), request("http://localhost:4000", {
-        application: application,
+}, request, _url, _application, crow = function(level, message) {
+    if (!_url) throw new Error("set the url for crow with crow.setUrl(<url>)");
+    if (!_application) throw new Error("set the application for crow with crow.setApplication(<application>)");
+    if (!message) throw new Error("log must contain a message");
+    level || console.warn("no log level specified. Defaulting to info"), request(_url, {
+        application: _application,
         level: level,
         message: message
     });
+    var consoleMessage = level.toUpperCase() + " - " + message;
+    switch (level) {
+      case "fatal":
+        console.error(consoleMessage);
+        break;
+
+      case "error":
+        console.error(consoleMessage);
+        break;
+
+      case "warn":
+        console.warn(consoleMessage);
+        break;
+
+      case "info":
+        console.info(consoleMessage);
+        break;
+
+      default:
+        console.log(consoleMessage);
+    }
+};
+
+crow.setUrl = function(url) {
+    _url = url;
+}, crow.setApplication = function(application) {
+    _application = application;
+}, crow.debug = function(message) {
+    crow("debug", message);
+}, crow.info = function(message) {
+    crow("info", message);
+}, crow.warn = function(message) {
+    crow("warn", message);
+}, crow.error = function(message) {
+    crow("error", message);
+}, crow.fatal = function(message) {
+    crow("fatal", message);
 };
     return crow;
   });
--- index.js
+++ index.js
@@ -1,19 +1,76 @@
+"use strict"; //jshint ignore:line
 
 var request;
+var _url, _application;
+
 /**
  * Send logs to woodpecker log server
- * @param {string} application
  * @param {string} level
  * @param {string} message
  */
-var crow = function(application, level, message) {
-  "use strict";
-console.log(request);
-  request('http://localhost:4000', {
-    application: application,
+var crow = function(level, message) {
+
+  if(!_url)
+    throw new Error('set the url for crow with crow.setUrl(<url>)');
+
+  if(!_application)
+    throw new Error('set the application for crow with crow.setApplication(<application>)');
+
+  if(!message)
+    throw new Error('log must contain a message');
+
+  if(!level)
+    console.warn('no log level specified. Defaulting to info');//jshint ignore:line
+
+  // Log to the log server
+  request(_url, {
+    application: _application,
     level: level,
     message: message
   });
-  // call request
+
+  // Log to the console.
+  // TODO restrict this to web only
+  var consoleMessage = level.toUpperCase() + ' - ' + message;
+
+  switch(level){
+    case 'fatal':
+      console.error(consoleMessage);//jshint ignore:line
+      break;
+    case 'error':
+      console.error(consoleMessage);//jshint ignore:line
+      break;
+    case 'warn':
+      console.warn(consoleMessage);//jshint ignore:line
+      break;
+    case 'info':
+      console.info(consoleMessage);//jshint ignore:line
+      break;
+    default:
+      console.log(consoleMessage);//jshint ignore:line
+  }
 };
 
+crow.setUrl = function(url) {
+  _url = url;
+};
+
+crow.setApplication = function(application) {
+  _application = application;
+};
+
+crow.debug = function (message) {
+  crow('debug', message);
+};
+crow.info = function (message) {
+  crow('info', message);
+};
+crow.warn = function (message) {
+  crow('warn', message);
+};
+crow.error = function (message) {
+  crow('error', message);
+};
+crow.fatal = function (message) {
+  crow('fatal', message);
+};
